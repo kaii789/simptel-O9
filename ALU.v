f40c3ab126a73ccf85d40fc7eba218d3e02c26c1
Kelvin Fan <kelvinfan001@gmail.com>
2019-03-19 18:05:25 -0400

    ALU module done

---
+++ ALU.v
@@ -0,0 +1,24 @@
+module ALU(
+	input clk,
+	input [31:0] src_A,
+	input [31:0] src_B,
+	input [2:0] ALU_control,
+	output zero,
+	output reg [31:0] ALU_result
+	);
+	
+	always @(*)
+	begin
+		case (ALU_control)
+		3'b000: ALU_result = src_A & src_B; // and
+		3'b001: ALU_result = src_A | src_A; // or
+		3'b010: ALU_result = a + b; // add
+		3'b110: ALU_result = a - b; // subtract
+		3'b111: begin if (a < b) ALU_result = 32'd1; // set on less than
+								else result = 32'd0;
+								end
+		default: result = a + b; // add
+		endcase
+	end
+	
+	assign zero = (ALU_result == 32'd0) ? 1'b1 : 1'b0; // set zero to be 0 if result is 0.
