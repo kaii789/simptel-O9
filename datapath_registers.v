e9d3df48f4afdf437a3afac9736734c287a690d0
Kelvin Fan <kelvinfan001@gmail.com>
2019-03-23 18:39:35 -0400

    fixed missing input

--- datapath_registers.v
+++ datapath_registers.v
@@ -1,6 +1,7 @@
 module Instruction_Register(clk, memory_data, IRWrite, instruc3, instruc2, instruc1, instruc0);
 	input clk;
 	input [31:0] memory_data;
+	input IRWrite;
 	output reg [5:0] instruc3;
 	output reg [4:0] instruc2, instruc1;
 	output reg [15:0] instruc0;
