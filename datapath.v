8a31e3cca0e2a50b434efbb042e734682b820587
Kelvin Fan <kelvinfan001@gmail.com>
2019-03-31 16:17:05 -0400

    conntected HEX displays to A

--- datapath.v
+++ datapath.v
@@ -14,7 +14,11 @@ module datapath(
 	RegDst,
 	clk,
 	reset,
-	opCode
+	opCode,
+	HEX0,
+	HEX1,
+	HEX2,
+	HEX3
 );
 
 
@@ -22,6 +26,7 @@ module datapath(
 	input [1:0] PCSource, ALUSrcB;
 	
 	output [5:0] opCode;
+	output [6:0] HEX0, HEX1, HEX2, HEX3;
 	
 	wire [31:0] ALU_Out_Reg;
 	wire [31:0] ALU_Out_Bus;
@@ -49,6 +54,26 @@ module datapath(
 	wire my_or_out;
 	wire [3:0] alu_decoder_out;
  	
+	hex_decoder H0(
+		.hex_digit(A[3:0]),
+		.segments(HEX0)
+	);
+	
+	hex_decoder H1(
+		.hex_digit(A[7:4]),
+		.segments(HEX1)
+	);
+	
+	hex_decoder H02(
+		.hex_digit(A[11:8]),
+		.segments(HEX2)
+	);
+	
+	hex_decoder H3(
+		.hex_digit(A[15:12]),
+		.segments(HEX3)
+	);
+	
 	three_to_one_mux mux_F(
 		.sel(PCSource[1:0]),
 		.in_0(ALU_Out_Bus[31:0]), // something weird here
@@ -271,4 +296,30 @@ module ALU_Register(in, out, clk);
 	always @(posedge clk)
 		out <= in[31:0];
 	
+endmodule
+
+module hex_decoder(hex_digit, segments);
+    input [3:0] hex_digit;
+    output reg [6:0] segments;
+   
+    always @(*)
+        case (hex_digit)
+            4'h0: segments = 7'b100_0000;
+            4'h1: segments = 7'b111_1001;
+            4'h2: segments = 7'b010_0100;
+            4'h3: segments = 7'b011_0000;
+            4'h4: segments = 7'b001_1001;
+            4'h5: segments = 7'b001_0010;
+            4'h6: segments = 7'b000_0010;
+            4'h7: segments = 7'b111_1000;
+            4'h8: segments = 7'b000_0000;
+            4'h9: segments = 7'b001_1000;
+            4'hA: segments = 7'b000_1000;
+            4'hB: segments = 7'b000_0011;
+            4'hC: segments = 7'b100_0110;
+            4'hD: segments = 7'b010_0001;
+            4'hE: segments = 7'b000_0110;
+            4'hF: segments = 7'b000_1110;   
+            default: segments = 7'h7f;
+        endcase
 endmodule
