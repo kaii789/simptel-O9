fe5e75a810d7f7f0730629c29559fd78c9f21c98
Kelvin Fan <kelvinfan001@gmail.com>
2019-03-30 22:46:49 -0400

    incurred some technical debt

--- datapath.v
+++ datapath.v
@@ -23,8 +23,8 @@ module datapath(
 	
 	output [5:0] opCode;
 	
+	wire [31:0] ALU_Out_Reg;
 	wire [31:0] ALU_Out_Bus;
-	wire [31:0] ALU_out_reg;
 	wire [31:0] instr_shifted;
 	wire [31:0] mux_F_out;
 	wire [31:0] PC_Out_Bus;
@@ -52,7 +52,7 @@ module datapath(
 	three_to_one_mux mux_F(
 		.sel(PCSource[1:0]),
 		.in_0(ALU_Out_Bus[31:0]), // something weird here
-		.in_1(ALU_out_reg[31:0]),
+		.in_1(ALU_Out_Reg[31:0]),
 		.in_2(sign_extend_B_out[31:0]),
 		.q(mux_F_out)
 	);
@@ -68,7 +68,7 @@ module datapath(
 	two_to_one_mux mux_A(
 		.sel(IorD),
 		.in_0(PC_Out_Bus[31:0]),
-		.in_1(ALU_out_reg[31:0]),
+		.in_1(ALU_Out_Bus[31:0]),
 		.q(mux_A_out[31:0])
 	);
 	
@@ -110,7 +110,7 @@ module datapath(
 	);
 	
 	two_to_one_mux mux_C(
-		.in_0(ALU_out_reg[31:0]),
+		.in_0(ALU_Out_Bus[31:0]),
 		.in_1(mem_out[31:0]),
 		.q(mux_C_out[31:0]),
 		.sel(MemtoReg)
@@ -167,10 +167,10 @@ module datapath(
 		.ALU_result(ALU_Out_Bus[31:0]) // not using a separate register here
 	);
 	
-	ALU_Register alu_reg(
-		.clk(clk),
+	ALU_Register alu_register(
 		.in(ALU_Out_Bus[31:0]),
-		.out(ALU_out_reg[31:0])
+		.clk(clk),
+		.out(ALU_Out_Reg[31:0])
 	);
 	
 	And_ my_and(
